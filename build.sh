713798310bca1b1a0d7911138e2892c1bf3916ac
Mark Piper <mark.piper@colorado.edu>
2026-03-10 12:50:24 -0600

    Install to tmp directory on debug build

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env bash
 
-build_type=Release
+build_type=Debug
 build_dir=_build
-dest_dir=$CONDA_PREFIX  # or $HOME/tmp
+
+if [ $build_type == "Debug" ]; then
+    dest_dir=$HOME/tmp/ghaas-debug
+else
+    dest_dir=$CONDA_PREFIX
+fi
 
 cmake -B $build_dir --fresh -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=$dest_dir
 cmake --build $build_dir
